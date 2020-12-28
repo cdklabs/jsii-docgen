3a444d2a6864cd1b9bdcf9bdd0748e3269cba477
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-12-28 09:46:29 -0500

    chore: address deprecations

--- .projenrc.js
+++ .projenrc.js
@@ -1,8 +1,8 @@
-const { TypeScriptLibraryProject, Semver } = require('projen');
+const { TypeScriptProject } = require('projen');
 
 const jsii = '1.9.0';
 
-const project = new TypeScriptLibraryProject({
+const project = new TypeScripProject({
   name: 'jsii-docgen',
   description: 'generates api docs for jsii modules',
   repository: 'https://github.com/eladb/jsii-docgen',
@@ -11,18 +11,18 @@ const project = new TypeScriptLibraryProject({
   bin: {
     'jsii-docgen': 'bin/jsii-docgen',
   },
-  devDependencies: {
-    '@types/node': Semver.caret('13.9.8'),
-    '@types/fs-extra': Semver.caret('8.1.0'),
-    'glob-promise': Semver.caret('3.4.0'),
-    'glob': Semver.caret('7.1.6'),
-  },
-  dependencies: {
-    'yargs': Semver.caret('15.4.1'),
-    'fs-extra': Semver.caret('9.0.0'),
-    'jsii-reflect': Semver.caret(jsii),
-    '@jsii/spec': Semver.caret(jsii),
-  },
+  devDeps: [
+    '@types/node@^13.9.8',
+    '@types/fs-extra@68.1.0',
+    'glob-promise@^3.4.0',
+    'glob@^7.1.6',
+  ],
+  deps: [
+    'yargs@^15.4.1',
+    'fs-extra@^9.0.0',
+    `jsii-reflect@^${jsii}`,
+    `@jsii/spec@^${jsii}`,
+  ],
   releaseToNpm: true,
   projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
 });
