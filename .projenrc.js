3effe2701f6720ee6b1eb7dff1eb89f5986a08d2
Elad Ben-Israel <benisrae@amazon.com>
2020-12-29 09:27:44 +0200

    chore: upgrade projen (#192)
    This PR upgrades projen to the latest version

--- .projenrc.js
+++ .projenrc.js
@@ -11,18 +11,17 @@ const project = new TypeScriptLibraryProject({
   bin: {
     'jsii-docgen': 'bin/jsii-docgen',
   },
-  devDependencies: {
-    '@types/node': Semver.caret('13.9.8'),
-    '@types/fs-extra': Semver.caret('8.1.0'),
-    'glob-promise': Semver.caret('3.4.0'),
-    'glob': Semver.caret('7.1.6'),
-  },
-  dependencies: {
-    'yargs': Semver.caret('15.4.1'),
-    'fs-extra': Semver.caret('9.0.0'),
-    'jsii-reflect': Semver.caret(jsii),
-    '@jsii/spec': Semver.caret(jsii),
-  },
+  devDeps: [
+    '@types/fs-extra@^8', // >8 needs a newer node version
+    'glob-promise',
+    'glob',
+  ],
+  deps: [
+    'yargs',
+    'fs-extra',
+    'jsii-reflect',
+    '@jsii/spec',
+  ],
   releaseToNpm: true,
   projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
 });
