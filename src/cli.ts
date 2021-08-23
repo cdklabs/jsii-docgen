38ef9c1764d89eacc324083559f0f2ef04ccf4b1
Christopher Rybicki <rybickic@amazon.com>
2021-08-23 14:23:15 -0700

    feat: allow specifying output language in CLI (#409)
    Allows the user to specify the CLI language via the `--language` flag.
    
    Also fixes the Python generated output to avoid writing arguments as multiple lines if they have no arguments, e.g.:
    
    ```python
    def to_json(
    
    )
    ```

--- src/cli.ts
+++ src/cli.ts
@@ -1,15 +1,18 @@
 import * as fs from 'fs';
 import * as yargs from 'yargs';
+import { Language } from './docgen/transpile/transpile';
 import { Documentation } from './index';
 
 export async function main() {
   const args = yargs
     .usage('Usage: $0')
     .option('output', { type: 'string', alias: 'o', required: false, desc: 'Output filename (defaults to API.md)' })
+    .option('language', { alias: 'l', default: 'typescript', choices: Language.values().map(x => x.toString()), desc: 'Output language' })
     .example('$0', 'Generate documentation for the current module as a single file (auto-resolves node depedencies)')
     .argv;
 
-  const docs = await Documentation.forProject(process.cwd());
+  const language = Language.fromString(args.language);
+  const docs = await Documentation.forProject(process.cwd(), { language });
   const output = args.output ?? 'API.md';
   const markdown = docs.render({ readme: false });
   fs.writeFileSync(output, markdown.render());
