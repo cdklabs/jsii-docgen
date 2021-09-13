25d1720b9f0134cc8fed281c94738f3096d08e56
Romain Marcadier <romain.muller@telecomnancy.net>
2021-09-14 01:29:02 +0200

    feat: improve signalling of ENOSPC during "npm install" (#437)
    Allow consumer code to react differently to failures caused by running out of
    disk space from other classes of failures. For example, this can be used to cut
    retries when the disk is full.
    
    This is done by recognizing the exit code returned by `npm install` when it hits
    `ENOSPC` and throwing a dedicated exception type in this scenario, so that
    consumers can catch, then `instanceof` test it.
    
    Related to cdklabs/construct-hub-webapp#387

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { Documentation } from './docgen/view/documentation';
 export { Language } from './docgen/transpile/transpile';
 export { UnsupportedLanguageError } from './docgen/transpile/transpile';
 export { TranspiledType } from './docgen/transpile/transpile';
+export * from './errors';
