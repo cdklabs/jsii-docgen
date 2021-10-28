d8b98570681b969e4c5a73e8af16c342abb33342
Romain Marcadier <romain.muller@telecomnancy.net>
2021-10-28 17:25:27 +0200

    chore: use `npm --json` mode (#476)
    In order to improve the reliability of error matching, use `npm` with
    the `--json` flag, which causes a JSON object to be the only thing
    output to `STDOUT`, which describes the result of the task (including
    error details if it failed). This is a lot more elegant than the log
    parsing we were previously doing.
    
    Additionally, check the version of the `npm` that is on `$PATH` using
    `npm version --json`, and use it if it is `npm >= v7`. This removes the
    need to install `npm@8` in scenarios where the platform provides
    sufficient tooling, which results in an increase of performance and
    reliability.

--- src/docgen/view/_npm.ts
+++ src/docgen/view/_npm.ts
@@ -1,18 +1,30 @@
 import { spawn, SpawnOptionsWithoutStdio } from 'child_process';
-import * as os from 'os';
 import { join } from 'path';
-import { NoSpaceLeftOnDevice, NpmError } from '../..';
+import { major } from 'semver';
+import { NoSpaceLeftOnDevice, NpmError } from '../../errors';
 
 export class Npm {
   #npmCommand: string | undefined;
 
-  public constructor(private readonly workingDirectory: string, private readonly logger = console.log) { }
+  public constructor(
+    private readonly workingDirectory: string,
+    private readonly logger = console.log,
+    npmCommand?: string,
+  ) {
+    this.#npmCommand = npmCommand;
+  }
 
+  /**
+   * Installs the designated package into this repository's working directory.
+   *
+   * @param target the name or path to the package that needs to be installed.
+   */
   public async install(target: string): Promise<void> {
-    return this.runCommand(
+    const result = await this.runCommand(
       await this.npmCommandPath(),
       [
         'install',
+        target,
         // this is critical from a security perspective to prevent
         // code execution as part of the install command using npm hooks. (e.g postInstall)
         '--ignore-scripts',
@@ -23,78 +35,165 @@ export class Npm {
         '--include=dev',
         // don't write or update a package-lock.json file
         '--no-package-lock',
-        target,
+        // always produce JSON output
+        '--json',
       ],
+      chunksToObject,
       {
         cwd: this.workingDirectory,
         shell: true,
       },
     );
+    return assertSuccess(result);
   }
 
+  /**
+   * Obtains the path to the npm command that should be run. This always returns
+   * the path to an npm >= 7, which "correctly" handles peerDependencies. If the
+   * npm version that's available in $PATH satisfies this predicate, this will
+   * simply return `npm`.
+   */
   private async npmCommandPath(): Promise<string> {
     if (this.#npmCommand) {
       return this.#npmCommand;
     }
+
+    try {
+      // If the npm in $PATH is >= v7, we can use that directly. The
+      // `npm version --json` command returns a JSON object containing the
+      // versions of several components (npm, node, v8, etc...). We are only
+      // interested in the `npm` key here.
+      const { exitCode, stdout } = await this.runCommand(
+        'npm', ['version', '--json'],
+        chunksToObject,
+      );
+      if (exitCode === 0 && major((stdout as any).npm) >= 7) {
+        return this.#npmCommand = 'npm';
+      }
+    } catch (e) {
+      this.logger('Could not determine version of npm in $PATH:', e);
+    }
+
     // npm@8 is needed so that we also install peerDependencies - they are needed to construct
     // the full type system.
-    this.logger('Installing npm@8...');
-    await this.runCommand(
+    this.logger('The npm in $PATH is not >= v7. Installing npm@8 locally...');
+    const result= await this.runCommand(
       'npm',
-      ['install', 'npm@8', '--no-package-lock', '--no-save'],
+      ['install', 'npm@8', '--no-package-lock', '--no-save', '--json'],
+      chunksToObject,
       {
         cwd: this.workingDirectory,
         shell: true,
-      });
+      },
+    );
+    assertSuccess(result);
+
     this.#npmCommand = join(this.workingDirectory, 'node_modules', '.bin', 'npm');
     this.logger(`Done installing npm@8 at ${this.#npmCommand}`);
     return this.#npmCommand;
   }
 
-  private async runCommand(command: string, args: readonly string[], options?: SpawnOptionsWithoutStdio): Promise<void> {
-    // Spawning before the promise so if the `spawn` call throws, runCommand's promise will reject
-    const child = spawn(command, args, { ...options, stdio: ['inherit', 'pipe', 'pipe'] });
-    return new Promise<void>((ok, ko) => {
+  /**
+   * Runs the supplied command with the provided arguments, captures the data
+   * pushed to STDOUT, and "parses" it using `outputTransform` to produce a
+   * result.
+   *
+   * You must consult the `exitCode` of the return value to determine whether
+   * the command was successful or not. Use the `assertSuccess` function to
+   * throw/reject in case the execution was not successful.
+   *
+   * @param command         the command to invoke.
+   * @param args            arguments to provide to the command.
+   * @param outputTransform the function that will parse STDOUT data.
+   * @param options         additional `spawn` options, if necessary.
+   */
+  private async runCommand<T = Buffer>(
+    command: string,
+    args: readonly string[],
+    outputTransform: (stderr: readonly Buffer[]) => T,
+    options?: SpawnOptionsWithoutStdio,
+  ): Promise<CommandResult<T>> {
+    return new Promise<CommandResult<T>>((ok, ko) => {
+      const child = spawn(command, args, { ...options, stdio: ['inherit', 'pipe', 'inherit'] });
       const stdout = new Array<Buffer>();
       child.stdout.on('data', (chunk) => {
-        process.stdout.write(chunk);
         stdout.push(Buffer.from(chunk));
-      });
-      const stderr = new Array<Buffer>();
-      child.stderr.on('data', (chunk) => {
-        process.stderr.write(chunk);
-        stderr.push(Buffer.from(chunk));
       });
 
       child.once('error', ko);
-      child.once('close', (code, signal) => {
+      child.once('close', (exitCode, signal) => {
         try {
-          if (code === 0) {
-            return ok();
-          }
-          const fullCommand = [command, ...args].join(' ');
-          if (signal != null) {
-            return ko(new NpmError(`Command "${fullCommand}" was killed by signal ${signal}`, { stdout, stderr }));
-          }
-          if (code === 228) {
-            // npm install exits with code 228 when it encounters the ENOSPC
-            // errno while doing it's business. This is effectively 256 - ENOSPC
-            // (which is 28). In this case, we'll throw a
-            // NoSpaceLeftOnDeviceError so that consumers can perform special
-            // handling if they so desire.
-            return ko(new NoSpaceLeftOnDevice(`No space left on device when running "${fullCommand}"`));
-          }
-          const maybeerrno = 256 - code!;
-          for (const [errname, errno] of Object.entries(os.constants.errno)) {
-            if (maybeerrno === errno) {
-              return ko(new NpmError(`Command "${fullCommand}' command exited with code ${code} (possibly ${errname})`, { stdout, stderr }));
-            }
-          }
-          return ko(new NpmError(`Command "${fullCommand}' command exited with code ${code}`, { stdout, stderr }));
-        } catch (unexpected) {
-          ko(unexpected);
+          ok({
+            command: `${command} ${args.join(' ')}`,
+            exitCode,
+            signal,
+            stdout: outputTransform(stdout),
+          });
+        } catch (error) {
+          ko(error);
         }
       });
     });
   }
 }
+
+interface CommandResult<T> {
+  readonly command: string;
+  readonly exitCode?: number;
+  readonly signal?: NodeJS.Signals;
+  readonly stdout: T;
+}
+interface SuccessfulCommandResult<T> extends CommandResult<T> {
+  readonly exitCode: 0;
+  readonly signal: undefined;
+}
+
+/**
+ * Asserts the provided CommandResult corresponds to a command that exited with
+ * code `0`. If that is not the case, this will throw an appropriate error,
+ * either `NpmError` or `NoSpaceLeftOnDevice`.
+ */
+function assertSuccess(result: CommandResult<ResponseObject>): asserts result is SuccessfulCommandResult<ResponseObject> {
+  const { command, exitCode, signal, stdout } = result;
+  if (exitCode === 0) {
+    return;
+  }
+  if (signal != null) {
+    throw new NpmError(`Command "${command}" was killed by ${signal}`, stdout);
+  }
+  if (exitCode === 228 || stdout.error?.code === 'ENOSPC') {
+    throw new NoSpaceLeftOnDevice(`Command "${command}" failed due to insufficient available disk space`);
+  }
+  const { code, detail, summary } = stdout.error;
+  const message = [
+    `Command "${command}" exited with code ${exitCode}`,
+    summary ? `: ${summary}` : '',
+    detail ? `\n${detail}` : '',
+    // If we have an error, but neither detail nor summary, then we probably
+    // have an actual Error object, so we'll stringify that here...
+    stdout.error && !detail && !summary ? `: ${stdout.error}` : '',
+  ].join('');
+  throw new NpmError(message, stdout, code);
+}
+
+/**
+ * Concatenates the provided chunks into a single Buffer, converts it to a
+ * string using the designated encoding, then JSON-parses it. If any part of
+ * this process results in an error, returns an object that contains the error
+ * and the raw chunks.
+ */
+function chunksToObject(chunks: readonly Buffer[], encoding = 'utf-8'): ResponseObject {
+  try {
+    return JSON.parse(Buffer.concat(chunks).toString(encoding));
+  } catch (error) {
+    return { error, raw: chunks };
+  }
+}
+
+type ResponseObject =
+  // The error when we failed to parse the output as JSON
+  | { readonly error: any; readonly raw: readonly Buffer[] }
+  // The error objects npm returns when operating in --json mode
+  | { readonly error: { readonly code: string; readonly summary: string; readonly detail: string } }
+  // The successful objects are treated as opaque blobs here
+  | { readonly error: undefined; readonly [key: string]: unknown };
