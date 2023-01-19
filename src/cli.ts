52f0ecca3f64a0465ba619256f63751c2d69a656
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-01-19 14:32:23 +0100

    feat!: support multiple languages as outputs
    BREAKING CHANGE: The output file you pass no longer should contain the file ending

--- src/cli.ts
+++ src/cli.ts
@@ -3,29 +3,48 @@ import * as yargs from 'yargs';
 import { Language } from './docgen/transpile/transpile';
 import { Documentation } from './index';
 
+type GenerateOptions = {
+  readme?: boolean;
+  language: Language;
+  submodule?: string;
+  allSubmodules?: boolean;
+}
+
+async function generateForLanguage(docs: Documentation, format: 'md' | 'json', options: GenerateOptions, output = 'API') {
+  const content = await (format === 'md' ? docs.toMarkdown(options) : docs.toJson(options));
+  const fileSuffix = format === 'md' ? 'md' : 'json';
+
+  fs.writeFileSync(`${output ?? 'API'}.${fileSuffix}`, content.render());
+}
+
+
 export async function main() {
   const args = yargs
     .usage('Usage: $0')
-    .option('output', { alias: 'o', type: 'string', required: false, desc: 'Output filename (defaults to API.md if format is markdown, and API.json if format is JSON)' })
+    .option('output', { alias: 'o', type: 'string', required: false, desc: 'Output filename, the file type is automatically added. (defaults to API.md if format is markdown, and API.json if format is JSON and one language is passed, API.language.md and API.language.json if multilpe are passed)' })
     .option('format', { alias: 'f', default: 'md', choices: ['md', 'json'], desc: 'Output format, markdown or json' })
-    .option('language', { alias: 'l', default: 'typescript', choices: Language.values().map(x => x.toString()), desc: 'Output language' })
+    .option('language', { array: true, alias: 'l', default: ['typescript'], choices: Language.values().map(x => x.toString()), desc: 'Output language' })
     .option('package', { alias: 'p', type: 'string', required: false, desc: 'The name@version of an NPM package to document', defaultDescription: 'The package in the current directory' })
     .option('submodule', { alias: 's', type: 'string', required: false, desc: 'Generate docs for a specific submodule (or "root")' })
     .example('$0', 'Generate documentation for the current module as a single file (auto-resolves node depedencies)')
     .argv;
 
-  const language = Language.fromString(args.language);
+
   const submodule = args.submodule === 'root' ? undefined : args.submodule;
   const allSubmodules = !args.submodule;
   const docs = await (args.package
     ? Documentation.forPackage(args.package)
     : Documentation.forProject(process.cwd()));
-  const options = { readme: false, language, submodule, allSubmodules };
-  const fileSuffix = args.format === 'md' ? 'md' : 'json';
-  const output = args.output ?? `API.${fileSuffix}`;
+  const options = (lang: string) => ({ readme: false, language: Language.fromString(lang), submodule, allSubmodules });
 
-  const content = await (args.format === 'md' ? docs.toMarkdown(options) : docs.toJson(options));
-  fs.writeFileSync(output, content.render());
+  if (args.language.length <= 1) {
+    await generateForLanguage(docs, args.format as 'md' | 'json', options(args.language[0]), args.output);
+  } else {
+    for (const lang of args.language) {
+      const output = `${args.output ?? 'API'}.${lang}`;
+      await generateForLanguage(docs, args.format as 'md' | 'json', options(lang), output);
+    }
+  }
 }
 
 main().catch(e => {
